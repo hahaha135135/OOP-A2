7b2f4efe4150d41e1581faa5e52a7c7130cd245c
hahaha135135 <623881354@qq.com>
2025-12-01 01:41:52 +0800

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,50 @@
+public abstract class Person {
+    // At least 3 instance variables
+    private String name;
+    private int age;
+    private String gender;
+    
+    // Default constructor
+    public Person() {
+        this.name = "Unknown";
+        this.age = 0;
+        this.gender = "Unknown";
+    }
+    
+    // Parameterized constructor
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    
+    // Getter and Setter methods
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+    
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Age: " + age + ", Gender: " + gender;
+    }
+}
